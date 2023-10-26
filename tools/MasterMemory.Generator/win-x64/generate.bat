08403046afcb480a8f9019cec749d3304d6dc789
nmskm240 <UTC.AM240@gmail.com>
2023-10-26 23:51:02 +0900

    add packages

---
+++ tools/MasterMemory.Generator/win-x64/generate.bat
@@ -0,0 +1,4 @@
+.\MasterMemory.Generator.exe ^
+    -i "..\..\..\client\Assets\Project\Scripts\Infra\Master" ^
+    -o "..\..\..\client\Assets\Project\Scripts\Infra\Master" ^
+    -n "Omino.Infra.Master"
