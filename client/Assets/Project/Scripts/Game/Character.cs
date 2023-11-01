2af6b270c40e078d29625c42705f65c1a746d073
nmskm240 <UTC.AM240@gmail.com>
2023-11-01 09:28:50 +0900

    idをもとにキャラクター画像を適用する

--- client/Assets/Project/Scripts/Game/Character.cs
+++ client/Assets/Project/Scripts/Game/Character.cs
@@ -1,11 +1,39 @@
+using Cysharp.Threading.Tasks;
 using DG.Tweening;
+using Omino.Infra.Master;
+using TMPro;
 using UnityEngine;
+using UnityEngine.U2D;
 
-public class Character : MonoBehaviour 
+public class Character : MonoBehaviour, IAsyncFactoryObject<CharacterData>
 {
-    private Collider _collider;
+    [SerializeField]
+    private SpriteRenderer _front;
+    [SerializeField]
+    private SpriteRenderer _back;
 
-    private void Start() {
+    private CharacterData _character;
+
+    // TODO:　設定用のクラスまたはマスターテーブルに移行させる
+    private string FrontSpriteResourcesPath { get { return string.Format("Sprite/Character/C_{0:d3}_Front", _character?.Id ?? 1); } }
+    private string BackSpriteResourcesPath { get { return string.Format("Sprite/Character/C_{0:d3}_Idle", _character?.Id ?? 1); } }
+    private string FrontSpriteName { get { return string.Format("C_{0:d3}_Front", _character?.Id ?? 1); } }
+    private string BackSpriteName { get { return string.Format("C_{0:d3}_Idle_0", _character?.Id ?? 1); } }
+
+    public async UniTaskVoid Setup(CharacterData data)
+    {
+        if (data == null)
+            return;
+
+        _character = data;
+        _front.sprite = await LoadCharacterSprite(FrontSpriteResourcesPath, FrontSpriteName);
+        _back.sprite = await LoadCharacterSprite(BackSpriteResourcesPath, BackSpriteName);
+    }
+
+    private async UniTask<Sprite> LoadCharacterSprite(string path, string spriteName)
+    {
+        var atlas = await Resources.LoadAsync<SpriteAtlas>(path) as SpriteAtlas;
+        return atlas?.GetSprite(spriteName);
     }
 }
 
